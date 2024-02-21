39a6a4602b1cbee3a966fa2e72506c1d6acd3108
atul-patel-silver <atulpatel2001a@gmail.com>
2024-02-21 20:14:57 +0530

    commit

--- ExamQuestionProject/QuizService/src/main/java/com/quiz/repository/QuizRepository.java
+++
@@ -1,8 +0,0 @@
-package com.quiz.repository;
-
-
-import com.quiz.entities.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizRepository extends JpaRepository<Quiz,Long> {
-}
